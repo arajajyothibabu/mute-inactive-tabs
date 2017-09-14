c0c2652f89abe4ec27c6892a065daf15a2a7b122
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-09-14 13:43:30 +0530

    more sophisticated supporting multiple windows

--- background.js
+++ background.js
@@ -3,31 +3,42 @@
  */
 
 var enabled = true;
-var title = "Mute Inactive Tabs";
-var disableTitle = "Disable Mute Inactive Tabs";
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     enabled = !enabled;
-    var currentTitle = enabled ? disableTitle : title;
-    chrome.browserAction.setTitle({title: currentTitle});
+    var t = enabled ? 'Unm' : 'M';
+    var d = enabled ? '' : 'd';
+    chrome.browserAction.setTitle({title: t + "ute Inactive Tabs"});
+    chrome.browserAction.setIcon({
+        path: {
+            '16': 'icons/icon' + d + '-16.png',
+            '32': 'icons/icon' + d + '-32.png',
+            '48': 'icons/icon' + d + '-48.png',
+            '128': 'icons/icon' + d + '-128.png'
+        }
+    });
+    if(!enabled){
+        chrome.tabs.query({}, function (tabs) {
+            tabs.forEach(function (tab) {
+                chrome.tabs.update(tab.id, {muted: false});
+            });
+        });
+    }
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     switch (request.type) {
         case "check-for-sound":
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) { //gets current Tab
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
                 var activeTab = tabs[0];
                 if(tabs.length > 0) {
                     chrome.tabs.update(activeTab.id, {muted: false});
                     if(enabled){
-                        if (activeTab.audible) { //if active tab has sound muting rest
+                        if (activeTab.audible) {
                             chrome.tabs.query({}, function (tabs) {
                                 tabs.forEach(function (tab) {
                                     if (tab.audible && tab.id !== activeTab.id) {
-                                        chrome.tabs.update(tab.id, {
-                                            muted: true
-                                        }, function (tab) {
-                                            //Address if any
-                                        });
+                                        chrome.tabs.update(tab.id, {muted: true});
                                     }
                                 });
                             });
